6569746e680b5adce57f8c144db9ce75600cc57a
Shobhit-Singhh <117563572+Shobhit-Singhh@users.noreply.github.com>
2025-02-24 01:44:18 +0530

    Update calendar.jsx

--- src/components/home/calendar.jsx
+++ src/components/home/calendar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CalendarSection = () => {
     return (
-        <div className="p-6">
+        <div className="">
             <div className="flex items-center gap-2 mb-4">
                 <h2 className="text-lg font-medium text-gray-800">Your Schedule</h2>
                 <span className="text-gray-400">→</span>
